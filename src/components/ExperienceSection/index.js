c5c186621273b4d7eae67e82ca1300ea59595da4
gracehjy <g_huang21@yahoo.com>
2024-07-20 18:57:45 -0500

    updated

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -39,8 +39,8 @@ const ExperienceSection = () => {
                 <Reveal>
                 <TopLine2>Washington University in St. Louis | Teaching Assistant</TopLine2>
                 <Description>
-                As a former TA for Logic & Discrete Mathematics and a current TA for Data Structures & Algorithms and Object-Oriented Software Development Laboratory, I spend most of my time at school conducting weekly office hours to answer students' questions and to assist them on problem sets, and I also grade and provide feedback on students' exams and assignments.
-                For Data Structures & Algorithms, in particular, I help students practice lecture material during weekly studio sessions.
+                As a former TA for Logic & Discrete Mathematics and a current TA for Data Structures & Algorithms and Object-Oriented Software Development Laboratory, I spend most of my time at school conducting weekly office hours to answer students' questions and to assist them on problem sets. I also grade and provide feedback on students' exams and assignments.
+                For Data Structures & Algorithms in particular, I help students practice lecture material during weekly studio sessions.
                 </Description>
                 </Reveal>
             </ExperienceCard>
@@ -52,8 +52,8 @@ const ExperienceSection = () => {
                 <Reveal>
                 <TopLine2>National Advanced Driving Simulator | Data Analyst Intern</TopLine2>
                 <Description>
-                Working at <Link to="https://dsri.uiowa.edu/">NADS</Link> as a high school intern, I spent most of my time learning from my peers and mentors. The simulations studied consisted of drivers taking on specific tasks and tracking where their eyes moved during those tasks. By the end of this internship, I had developed a batch-processing method 
-                using Python to clean 1000+ unstructured eye-tracking pixel data, and I had determined generalized areas where the drivers would frequently glance to by dividing the pixel data into quadrants and visualizing that data using various statistical models.
+                Working at <Link to="https://dsri.uiowa.edu/">NADS</Link> as a high school intern, I spent most of my time learning from my peers and mentors. The simulations studied consisted of drivers taking on specific tasks and researchers tracking where the drivers' eyes moved during those tasks. By the end of this internship, I had developed a batch-processing method 
+                using Python to clean 1000+ unstructured eye-tracking pixel data, and I had identified the primary areas of visual distraction amongst drivers by dividing the pixel data into quadrants and visualizing that data using various statistical models.
                 </Description>
                 </Reveal>
             </ExperienceCard>
